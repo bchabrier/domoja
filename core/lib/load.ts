fd7731272ed0bcb6f818d6f18b8d901947b99e9d
Bruno Chabrier <bruno_chabrier@hotmail.com>
2018-12-16 00:02:41 +0100

    Added isReleased to Sandbox

--- core/lib/load.ts
+++ core/lib/load.ts
@@ -79,7 +79,6 @@ type user = {
 */
 
 type Sandbox = {
-    isReleased(): boolean, // indicates if the sandbox is in released state
     console: typeof console,
     assert: typeof assert,
     require: typeof require,
@@ -116,7 +115,6 @@ export class ConfigLoader extends events.EventEmitter {
     rootModule: Module;
 
     private sandbox: Sandbox = {
-        isReleased: () => {return this.released},
         console: console,
         require: require,
         assert: assert,
